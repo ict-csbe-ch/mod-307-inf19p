d77182fa9eb7935263b2a14d4f3eb8e9c846d797
Nathan Fahrni <5468107-nFahrni@users.noreply.gitlab.com>
2021-01-22 10:56:55 +0100

    Login HTML finished Aaron you can start your work now

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [str(BASE_DIR.joinpath('templates'))]
+
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -101,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-    {
+   {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
@@ -125,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ Including another URL_conf
 from django.contrib import admin
 from django.urls import path
 
-from my_app import views
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
