12182a3602709c242a335048c36e6b6a7fce78f1
DMCsBe <dav.maz321@gmail.com>
2021-02-05 09:36:28 +0100

    going back to old version

--- cronjob/apps.py
+++ cronjob/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class CronjobConfig(AppConfig):
+class MyAppConfig(AppConfig):
     name = 'cronjob'
--- cronjob/models.py
+++ cronjob/models.py
@@ -1,32 +1,5 @@
 from django.db import models
 
-
-class Cronjob(models.Model):
-    title = models.CharField(max_length=255,  null=False)
-    address = models.CharField(max_length=255,  null=False)
-    notification = models.CharField(max_length=45,  null=False)
-    minute = models.CharField(max_length=45)
-    hour = models.CharField(max_length=45)
-    weekday = models.CharField(max_length=45)
-    month = models.CharField(max_length=45)
-    day = models.CharField(max_length=45)
-    user = models.ForeignKey("User", on_delete=models.RESTRICT)
-
-
-class Address(models.Model):
-    street = models.CharField(max_length=255,  null=False)
-    strassenNr = models.CharField(max_length=10,  null=False)
-    plz = models.ForeignKey("Ort", on_delete=models.RESTRICT)
-
-
-class Ort(models.Model):
-    plz = models.CharField(max_length=25, null=False)
-    name = models.CharField(max_length=255, null=False)
-
-
-class User(models.Model):
-    email = models.CharField(max_length=25, null=False)
-    firstName = models.CharField(max_length=25, null=False)
-    lastName = models.CharField(max_length=25, null=False)
-    password = models.CharField(max_length=25, null=False)
-    address = models.ForeignKey("Address", on_delete=models.RESTRICT)
+# Create your models here.
+firstname = models.CharField(max_length=255)
+lastname = models.CharField(max_length=255)
